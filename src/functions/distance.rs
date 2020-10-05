d6ce7308298e217e302faa28336fca9747786f0d
Jay Kickliter <jay@kickliter.com>
2020-10-05 11:37:54 -0700

    Simplify euclidean distance

--- src/functions/distance.rs
+++ src/functions/distance.rs
@@ -1,21 +1,18 @@
-/// Node distance calculation functions
-///
-///
+//! Node distance calculation functions.
 
-use ndarray;
 use ndarray::ArrayView1;
 
-// Returns the euclidian distance between 2 vectors
+/// Returns the Euclidean distance between `a` and `b`.
 pub fn euclid_dist(a: ArrayView1<f64>, b: ArrayView1<f64>) -> f64 {
-    if a.len() != b.len() {
-        panic!("Both arrays must be of same length to find Euclidian distance!");
-    }
+    debug_assert_eq!(
+        a.len(),
+        b.len(),
+        "Euclidean distance requires equally length inputs"
+    );
 
-    let mut dist: f64 = 0.0;
-
-    for i in 0..a.len() {
-        dist += (a[i] - b[i]).powf(2.0);
-    }
-
-    dist.powf(0.5)
+    a.iter()
+        .zip(b.iter())
+        .map(|(a, b)| (a - b).powi(2))
+        .sum::<f64>()
+        .sqrt()
 }
